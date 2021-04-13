0f474bce7f6bcc1a0780f498b24caf2af9dbc0f1
Phan Van Nhan <nhanphanvan99@gmail.com>
2021-04-13 22:36:38 +0700

    create showtime manager page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import AdminPage from "./pages/admin/AdminPage"
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AdminPage from "./pages/admin/AdminPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import routes from "./routes/index.route";
 import HomePage from "./pages/HomePage/HomePage"
 import UserManagementPage from './pages/admin/UserManagementPage';
 import MovieManager from './pages/manager/movie manager';
 import ActorManager from './pages/manager/ActorManager/index';
 import ManagerPage from './pages/manager/index';
+import HomePage from "./pages/HomePage/HomePage";
+import UserManagementPage from "./pages/admin/UserManagementPage";
+import MovieManager from "./pages/manager/movie manager";
+import ShowTimeManager from "./pages/ShowTimeManager/HomePage/HomePage";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/admin"> 
+          <Route exact path="/admin">
             <AdminPage></AdminPage>
           </Route>
-          <Route exact path="/admin/users"> 
+          <Route exact path="/admin/users">
             <UserManagementPage></UserManagementPage>
           </Route>
-          <Route exact path="/"> 
+          <Route exact path="/">
             <HomePage></HomePage>
           </Route>
           <Route exact path="/manager"> 
             <ManagerPage></ManagerPage>
+          </Route>  
+          <Route exact path="/manager/movies">
+            <MovieManager></MovieManager>
+          </Route>
+          <Route exact path="/showtime_manager">
+            <ShowTimeManager></ShowTimeManager>
           </Route>
         </Switch>
       </Router>
