d71e3c6eac13820d8e40fc5a531a2d62a246ae88
PhanVu <phanvu200470@gmail.com>
2021-04-14 09:30:07 +0700

    Create function AddNewActor

--- src/reducers/ActorReducer/ActorReducer.js
+++ src/reducers/ActorReducer/ActorReducer.js
@@ -6,6 +6,7 @@ var initialState = [
         id: 1,
         name: "Phan Văn Vũ",
         birthday: "10/10/1999",
+        nationality: "Việt Nam",
         image: "",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -13,6 +14,7 @@ var initialState = [
         id: 2,
         name: "Phan Văn Anh",
         image: "",
+        nationality: "Việt Nam",
         birthday: "10/10/1999",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -20,6 +22,7 @@ var initialState = [
         id: 3,
         name: "Phan Văn Nam",
         image: "",
+        nationality: "Việt Nam",
         birthday: "10/10/1999",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -27,6 +30,7 @@ var initialState = [
         id: 4,
         name: "Phan Văn Tài",
         image: "",
+        nationality: "Việt Nam",
         birthday: "10/10/1999",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -34,6 +38,7 @@ var initialState = [
         id: 5,
         name: "Phan Văn Thành",
         image: "",
+        nationality: "Việt Nam",
         birthday: "10/10/1999",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -41,6 +46,7 @@ var initialState = [
         id: 6,
         name: "Phan Văn Tạo",
         image: "",
+        nationality: "Việt Nam",
         birthday: "10/10/1999",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -48,6 +54,7 @@ var initialState = [
         id: 7,
         name: "Phan Văn Vũ",
         image: "",
+        nationality: "Việt Nam",
         birthday: "10/10/1999",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -55,6 +62,7 @@ var initialState = [
         id: 8,
         name: "Phan Văn Anh Nam",
         image: "",
+        nationality: "Việt Nam",
         birthday: "10/10/1999",
         description: "Trấn Thành tên khai sinh Huỳnh Trấn Thành (sinh ngày 5 tháng 2 năm 1987) là một diễn viên hài, người dẫn chương trình, diễn viên lồng tiếng và diễn viên điện ảnh người Việt Nam. Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh Xem thêm tại: https://www.galaxycine.vn/dien-vien/tran-thanh"
     },
@@ -62,49 +70,48 @@ var initialState = [
     
 ];
 
-// const findIndex = (users, id) => {
-//     var result = -1;
-//     users.forEach((user, index) => {
-//         if(user.id === id ){
-//             result = index;
-//         }
-//     });
-//     return result;
-// }
+const findIndex = (actors, id) => {
+    var result = -1;
+    actors.forEach((actor, index) => {
+        if(actor.id === id ){
+            result = index;
+        }
+    });
+    return result;
+}
 
-// const randomId = () =>{
-//     return Math.floor(Math.random() * 100) + 1;
-// }
+const randomId = () =>{
+    return Math.floor(Math.random() * 100) + 1;
+}
 
 var myReducer = (state = initialState, action) => {
     var index = -1;
     switch(action.type){
         case types.LIST_ALL_ACTORS:
             return state;
-        // case types.SAVE_USER:
-        //     var today = new Date();
-        //     var role = parseInt(action.user.role);
-        //     var newUser = {
-        //         id: action.user.id,
-        //         username: action.user.username,
-        //         password: action.user.password,
-        //         role: role,
-        //         createdAt : + today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear(),
-        //         status: action.user.status === 'true' ? true: false
-        //     }
-        //     if(!newUser.id){
-        //         newUser.id = randomId();
-        //         state.push(newUser);
-        //     } else {
-        //         index = findIndex(state, newUser.id);
-        //         let editUser = {...state[index]};
-        //         editUser.username = newUser.username;
-        //         editUser.role = newUser.role;
-        //         editUser.status = newUser.status;
-        //         state[index] = editUser;
-        //         //return state;
-        //     }
-        //     return [...state];    
+        case types.SAVE_ACTOR:
+            var newActor = {
+                id: action.actor.id,
+                name: action.actor.name,
+                birthday: action.actor.birthday,
+                nationality: action.actor.nationality,
+                image: action.actor.image,
+                description : action.actor.description,
+            }
+            if(!newActor.id){
+                newActor.id = randomId();
+                state.push(newActor);
+            } else {
+                index = findIndex(state, newActor.id);
+                let editActor = {...state[index]};
+                editActor.name = newActor.name;
+                editActor.image = newActor.image;
+                editActor.birthday = newActor.birthday;
+                editActor.description = newActor.description;
+                editActor.nationality = newActor.nationality;
+                state[index] = editActor;
+            }
+            return [...state];    
         // case types.DELETE_USER:
         //     index = findIndex(state, action.id)
         //     state.splice(index, 1);
