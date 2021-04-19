b797851a1190ef85d918cc92f2f7c0cba062175a
Phan Van Nhan <nhanphanvan99@gmail.com>
2021-04-19 21:53:59 +0700

    create edit showtime page

--- src/constants/ActionType.js
+++ src/constants/ActionType.js
@@ -1,34 +1,36 @@
 // Users manager
-export const LIST_ALL_USERS = 'LIST_ALL_USERS';
-export const SAVE_USER = 'SAVE_USER';
-export const TOGGLE_USER_FORM = 'TOGGLE_USER_FORM';
-export const DELETE_USER = 'DELETE_USER';
-export const GET_USER_EDITING = 'GET_USER_EDITING';
-export const UPDATE_USER_STATUS = 'UPDATE_USER_STATUS';
-export const FILTER_USER = 'FILTER_USER';
+export const LIST_ALL_USERS = "LIST_ALL_USERS";
+export const SAVE_USER = "SAVE_USER";
+export const TOGGLE_USER_FORM = "TOGGLE_USER_FORM";
+export const DELETE_USER = "DELETE_USER";
+export const GET_USER_EDITING = "GET_USER_EDITING";
+export const UPDATE_USER_STATUS = "UPDATE_USER_STATUS";
+export const FILTER_USER = "FILTER_USER";
 
 // Movies manager
 
-export const LIST_ALL_MOVIES = 'LIST_ALL_MOVIES';
-export const SAVE_MOVIE = 'SAVE_MOVIE';
-export const TOGGLE_MOVIE_MODAL = 'TOGGLE_MOVIE_MODAL';
-export const TOGGLE_MOVIE_FORM = 'TOGGLE_MOVIE_FORM';
-export const DELETE_MOVIE = 'DELETE_MOVIE';
-export const GET_MOVIE_INFO = 'GET_MOVIE_INFO';
-export const UPDATE_MOVIE = 'UPDATE_MOVIE';
-export const FILTER_MOVIE = 'FILTER_MOVIE';
+export const LIST_ALL_MOVIES = "LIST_ALL_MOVIES";
+export const SAVE_MOVIE = "SAVE_MOVIE";
+export const TOGGLE_MOVIE_MODAL = "TOGGLE_MOVIE_MODAL";
+export const TOGGLE_MOVIE_FORM = "TOGGLE_MOVIE_FORM";
+export const DELETE_MOVIE = "DELETE_MOVIE";
+export const GET_MOVIE_INFO = "GET_MOVIE_INFO";
+export const UPDATE_MOVIE = "UPDATE_MOVIE";
+export const FILTER_MOVIE = "FILTER_MOVIE";
 
 // Genres
-export const LIST_ALL_GENRES = 'LIST_ALL_GENRES';
+export const LIST_ALL_GENRES = "LIST_ALL_GENRES";
 
 // Actors
 
-export const LIST_ALL_ACTORS = 'LIST_ALL_ACTORS';
-export const SAVE_ACTOR = 'SAVE_ACTOR';
-export const TOGGLE_ACTOR_MODAL = 'TOGGLE_ACTOR_MODAL';
-export const TOGGLE_ACTOR_FORM = 'TOGGLE_ACTOR_FORM';
-export const DELETE_ACTOR = 'DELETE_ACTOR';
-export const GET_ACTOR_INFO = 'GET_ACTOR_INFO';
-export const UPDATE_ACTOR = 'UPDATE_ACTOR';
-export const FILTER_ACTOR = 'FILTER_ACTOR';
+export const LIST_ALL_ACTORS = "LIST_ALL_ACTORS";
+export const SAVE_ACTOR = "SAVE_ACTOR";
+export const TOGGLE_ACTOR_MODAL = "TOGGLE_ACTOR_MODAL";
+export const TOGGLE_ACTOR_FORM = "TOGGLE_ACTOR_FORM";
+export const DELETE_ACTOR = "DELETE_ACTOR";
+export const GET_ACTOR_INFO = "GET_ACTOR_INFO";
+export const UPDATE_ACTOR = "UPDATE_ACTOR";
+export const FILTER_ACTOR = "FILTER_ACTOR";
 
+export const FETCH_DATA_CINEMA = "FETCH_DATA_CINEMA";
+export const FETCH_DATA_SHOWTIME = "FETCH_DATA_SHOWTIME";
---
+++ src/reducers/ShowTimeReducer/ShowTimeReducer.js
@@ -0,0 +1,54 @@
+import * as types from "../../constants/ActionType";
+
+var initialState = {
+  cinema: [
+    {
+      id: "1",
+      name: "cinema Da Nang 1",
+      address: "Hung Vuong 1",
+      numOfRoom: 4,
+    },
+    {
+      id: "2",
+      name: "cinema Da Nang 2",
+      address: "Hung Vuong 2",
+      numOfRoom: 4,
+    },
+    {
+      id: "3",
+      name: "cinema Da Nang 3",
+      address: "Hung Vuong 3",
+      numOfRoom: 4,
+    },
+    {
+      id: "4",
+      name: "cinema Da Nang 4",
+      address: "Hung Vuong 4",
+      numOfRoom: 4,
+    },
+  ],
+  showtime: ["08:00", "13:00", "15:00", "17:00", "20:00"],
+};
+
+function reducerShowTime(state = initialState, action) {
+  switch (action.type) {
+    case types.FETCH_DATA_CINEMA: {
+      return {
+        ...state,
+        cinema: action.cinema,
+      };
+    }
+
+    case types.FETCH_DATA_SHOWTIME: {
+      return {
+        ...state,
+        showtime: action.showtime,
+      };
+    }
+
+    default:
+      return state;
+  }
+}
+
+export default reducerShowTime;
