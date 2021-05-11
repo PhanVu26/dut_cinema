a6349bb040e0be6b1d34af3274f897247a3f5b52
Phan Van Nhan <nhanphanvan99@gmail.com>
2021-05-11 22:50:51 +0700

    Fix MoviePage Error

--- src/utils/ApiCallerServer.js
+++ src/utils/ApiCallerServer.js
@@ -1,12 +1,32 @@
 import axios from "axios";
 import * as Config from "../constants/Config";
 
-export default function callApi (endpoint, method = "GET", body) {
-  return axios({
-    method: method,
-    url: `${Config.API_URL}/${endpoint}`,
-    data: body
-  }).catch(err => {
-    alert(err.response);
-  });
+export default function callApi(endpoint, method = "GET", body) {
+  if (!localStorage.getItem("account")) {
+    localStorage.setItem("account", JSON.stringify(""));
+  }
+  let account = JSON.parse(localStorage.getItem("account"));
+  if (Object.keys(account).length === 0) {
+    return axios({
+      method: method,
+      url: `${Config.API_URL}/${endpoint}`,
+      data: body,
+    }).catch((err) => {
+      console.log(err);
+    });
+  } else {
+    const authAxios = axios.create({
+      baseURL: Config.API_URL,
+      headers: {
+        Authorization: "Bearer " + account.accessToken,
+      },
+    });
+    return authAxios({
+      method: method,
+      url: `${Config.API_URL}/${endpoint}`,
+      data: body,
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 }
