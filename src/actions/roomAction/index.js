865d3094ae429116ef21171c065ff2bc4a85dcb9
PHANVU-2601\DELL <phanvu200470@gmail.com>
2021-08-16 22:06:24 +0700

    Add loading layer in room management page

--- src/actions/roomAction/index.js
+++ src/actions/roomAction/index.js
@@ -5,6 +5,7 @@ import callApi from "../../utils/ApiCallerServer";
 
 export const actFetchDataRoomsRequest = (id) => {
     return (dispatch) => {
+        dispatch(loadRoom());
       return callApi(`cinemas/${id}/rooms?page=1&perPage=1000`, "GET", null).then((res) => {
         console.log("cinemas", res.data)
         dispatch(actFetchDataRooms(res.data.rooms));
@@ -22,6 +23,7 @@ export const actFetchDataRooms = (rooms) => {
 
 export const actSaveRoomRequest = (room, id) => {
     return (dispatch) => {
+        dispatch(loadRoom());
         return callApi(`cinemas/${id}/rooms`, "POST", room).then((res) => {
             alert("Thêm phòng thành công")
             dispatch(saveRoom(res.data.rooms));
@@ -56,6 +58,7 @@ export const getRoomInfo = (room) => {
 
 export const actDeleteRoomRequest = (id) => {
     return (dispatch) => {
+        dispatch(loadRoom());
         return callApi(`rooms/${id}`, "DELETE", null).then((res) => {
             alert("Xóa phòng thành công")
             dispatch(deleteRoom(id));
@@ -75,6 +78,7 @@ export const deleteRoom = (id) => {
 
 export const actUpdateRoomRequest = (room) => {
     return (dispatch) => {
+        dispatch(loadRoom());
         return callApi(`rooms/${room.id}`, "PATCH", room).then((res) => {
             alert("Cập nhật thành công.")
             dispatch(actUpdateRoom(res.data));
