9d89bc0423fe949968a9215bfe6a65f6bcf12174
PHANVU-2601\DELL <phanvu200470@gmail.com>
2021-08-16 11:10:07 +0700

    Change actor management to using material ui table

--- src/pages/manager/index.js
+++ src/pages/manager/index.js
@@ -6,6 +6,7 @@ import {
 import MovieManager from './MovieManager/index';
 import MoviesPage from './MovieManager/MoviesPage';
 import ActorManager from './ActorManager/index';
+import ActorsPage from './ActorManager/ActorsPage';
 import UserProfile from '../UserProfile/UserProfile';
 import Navbar from '../../components/admin/Navbar/Navbar';
 import Footer from '../../components/admin/Footer/Footer';
@@ -34,7 +35,7 @@ class ManagerPage extends Component{
             <Switch>
                     <Route path="/manager" exact component={UserProfile}></Route> 
                     <Route path="/manager/movies" exact component={MoviesPage}></Route>  
-                    <Route path="/manager/actors" exact component={ActorManager}></Route>  
+                    <Route path="/manager/actors" exact component={ActorsPage}></Route>  
                     <Route path="/manager/profile" exact component={UserProfile}></Route>  
             </Switch>
             <Footer></Footer>
