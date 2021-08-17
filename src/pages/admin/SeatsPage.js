e84ce93a66a77ecc51401a3ef20343239ed94559
PHANVU-2601\DELL <phanvu200470@gmail.com>
2021-08-17 21:22:55 +0700

    Change seat management to using material ui table

---
+++ src/pages/admin/SeatsPage.js
@@ -0,0 +1,322 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Paper from "@material-ui/core/Paper";
+import Loader from "react-loader-advanced";
+
+import * as actions from "../../actions/seatAction/index";
+import SeatControl from "../../components/Control/SeatControl/SeatControl";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import SearchIcon from "@material-ui/icons/Search";
+import SeatForm from "../../components/Modal/SeatModal/SeatForm";
+import { NavLink } from "react-router-dom";
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  { id: "row", numeric: false, disablePadding: true, label: "Hàng ghế" },
+  { id: "quantity", numeric: false, disablePadding: false, label: "Số lượng" },
+];
+
+function EnhancedTableHead(props) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align="center"
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  searchBar: {
+    border: 1,
+  },
+  root: {
+    width: "100%",
+  },
+  paper: {
+    width: "100%",
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState("desc");
+  const [orderBy, setOrderBy] = React.useState("id");
+  const [page, setPage] = React.useState(0);
+  const [seatFilter, setSeatFilter] = React.useState({});
+  const [rows, setRows] = React.useState([]);
+  const [dense, setDense] = React.useState(false);
+  const loading = useSelector((state) => state.seats.loading);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  var seats = useSelector((state) => state.seats.seats);
+  const roomId = window.location.pathname.split('/')[3];
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(actions.actFetchDataSeatsRequest(parseInt(roomId)));
+    setRows(preprocessData(seats));
+  }, []);
+
+  useEffect(() => {
+      
+    setRows(preprocessData(seats));
+  }, [seats]);
+
+
+  function preprocessData(seats){
+    const data = seats.reduce((prev, curr) => (prev[curr.row] = ++prev[curr.row] || 1, prev), {});
+    var results = [];
+    const keys = Object.keys(data);
+    const values = Object.values(data);
+    for(let i = 0 ; i < keys.length; i ++){
+        results.push({
+            row: keys[i],
+            quantity: values[i]
+        })
+    }
+    return results;    
+  }
+
+  
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const refreshData = () => {
+    dispatch(actions.actFetchDataSeatsRequest(roomId));
+    setSeatFilter({ seatRows: ""});
+  };
+
+  const searchSeatQuery = (e) => {
+    e.preventDefault();
+    // const filter = `filter={"roomNumber": {"like": "${SeatFilter.roomNumber}"}}`;
+    // dispatch(actions.actFetchDataCinemasFilterRequest(filter));
+  };
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <Loader show={loading} message={"Loading......."}>
+      <section style={{ backgroundColor: "#f3f3f4" }}>
+        <SeatForm></SeatForm>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
+              <div class={"row " + classes.searchBar}>
+                <div class="col-xl-12 col-12 mb-xl-0">
+                  <h3 class="text-left mb-2 pt-3">Quản lý ghế</h3>
+                  <div className="mb-3 mt-3">
+                    <div
+                      className="col-12"
+                      style={{
+                        boxShadow:
+                          "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
+                        backgroundColor: "white",
+                        borderRadius: "4px",
+                      }}
+                    >
+                      <form
+                        class="form-inline pt-3 pb-3"
+                        onSubmit={searchSeatQuery}
+                      >
+                        <div class="form-group mb-2 mr-5">
+                          <lable>Hàng ghế:</lable>&nbsp;
+                          <input
+                            className="form-control"
+                            placeholder="Nhập hàng ghế"
+                            value={seatFilter.seatRows}
+                            onChange={(e) => {
+                              setSeatFilter({
+                                ...seatFilter,
+                                seatRows: e.target.value,
+                              });
+                            }}
+                          ></input>
+                        </div>
+                        
+                        <div class="form-group mb-2">
+                          <button type="submit" className="btn btn-primary">
+                            <SearchIcon>Tìm kiếm</SearchIcon>
+                          </button>
+                          &nbsp;
+                          <button
+                            className="btn btn-warning"
+                            onClick={() => {
+                              refreshData();
+                            }}
+                          >
+                            <RefreshIcon color="secondary">Làm mới</RefreshIcon>
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div className={classes.root}>
+                    <SeatControl></SeatControl>
+                    <Paper className={classes.paper}>
+                      <TableContainer>
+                        <Table
+                          className={classes.table}
+                          aria-labelledby="tableTitle"
+                          size={dense ? "small" : "medium"}
+                          aria-label="enhanced table"
+                        >
+                          <EnhancedTableHead
+                            classes={classes}
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                          />
+                          <TableBody>
+                            {stableSort(rows, getComparator(order, orderBy))
+                              .slice(
+                                page * rowsPerPage,
+                                page * rowsPerPage + rowsPerPage
+                              )
+                              .map((row, index) => {
+                                const labelId = `enhanced-table-checkbox-${index}`;
+                                return (
+                                  <TableRow>
+                                    <TableCell
+                                      component="th"
+                                      id={labelId}
+                                      scope="row"
+                                      align="center"
+                                    >
+                                      {row.row}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                      {row.quantity}
+                                    </TableCell>
+                                  </TableRow>
+                                );
+                              })}
+                            {emptyRows > 0 && (
+                              <TableRow
+                                style={{
+                                  height: (dense ? 33 : 53) * emptyRows,
+                                }}
+                              >
+                                <TableCell colSpan={6} />
+                              </TableRow>
+                            )}
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
+                      <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                      />
+                    </Paper>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Loader>
+  );
+}
