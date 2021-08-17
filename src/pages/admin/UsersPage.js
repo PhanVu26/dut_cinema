d0d7a78219a745be54a5e17513fca186e1eda697
PHANVU-2601\DELL <phanvu200470@gmail.com>
2021-08-17 21:50:55 +0700

    Add style icon breadcrumb in user page

--- src/pages/admin/UsersPage.js
+++ src/pages/admin/UsersPage.js
@@ -12,6 +12,11 @@ import TableRow from "@material-ui/core/TableRow";
 import TableSortLabel from "@material-ui/core/TableSortLabel";
 import Paper from "@material-ui/core/Paper";
 import Loader from 'react-loader-advanced';
+import { NavLink } from "react-router-dom";
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import HomeIcon from '@material-ui/icons/Home';
+import GrainIcon from '@material-ui/icons/Grain';
 
 import * as userActions from "../../actions/userManager/userAction";
 import UserControl from "../../components/Control/UserControl/UserControl";
@@ -130,8 +135,21 @@ const useStyles = makeStyles((theme) => ({
     width: 1,
   },
   container: {
-    maxHeight: 280,
+    maxHeight: 289
   },
+  link: {
+    display: 'flex',
+  },
+  icon: {
+    marginRight: theme.spacing(0.5),
+    width: 20,
+    height: 20,
+  },
+  breadcrumb: {
+    height: '10px',
+    backgroundColor: '#f3f3f4',
+    paddingLeft: '0px'
+  }
 }));
 
 export default function EnhancedTable() {
@@ -252,7 +270,21 @@ export default function EnhancedTable() {
           <div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
             <div class={"row " + classes.searchBar}>
               <div class="col-xl-12 col-12 mb-xl-0">
-                <h3 class="text-left mb-2 pt-3">Danh sách người dùng</h3>
+              <Breadcrumbs aria-label="breadcrumb" className={classes.breadcrumb}>
+                    <NavLink
+                      
+                      to={"/admin"}
+                      
+                      className={classes.link}
+                    >
+                      <HomeIcon className={classes.icon} />
+                      Trang chủ
+                    </NavLink>
+                    <Typography color="textPrimary" className={classes.link}>
+                      <GrainIcon className={classes.icon} />
+                      Người dùng
+                    </Typography>
+                  </Breadcrumbs>
                 <div className="mb-3 mt-3">
                   <div 
                     className="col-12" 
