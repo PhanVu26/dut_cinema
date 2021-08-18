e03fe14745364a50610860e531e2f9e110167e2d
PHANVU-2601\DELL <phanvu200470@gmail.com>
2021-08-18 09:32:57 +0700

    Add breadcrumb for transaction page

--- src/pages/admin/TransactionsPage.js
+++ src/pages/admin/TransactionsPage.js
@@ -12,6 +12,11 @@ import TableRow from "@material-ui/core/TableRow";
 import TableSortLabel from "@material-ui/core/TableSortLabel";
 import Paper from "@material-ui/core/Paper";
 import Loader from 'react-loader-advanced';
+import { NavLink } from "react-router-dom";
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import HomeIcon from '@material-ui/icons/Home';
+import GrainIcon from '@material-ui/icons/Grain';
 
 import RefreshIcon from '@material-ui/icons/Refresh';
 import SearchIcon from '@material-ui/icons/Search';
@@ -128,6 +133,19 @@ const useStyles = makeStyles((theme) => ({
     top: 20,
     width: 1,
   },
+  link: {
+    display: 'flex',
+  },
+  icon: {
+    marginRight: theme.spacing(0.5),
+    width: 20,
+    height: 20,
+  },
+  breadcrumb: {
+    height: '10px',
+    backgroundColor: '#f3f3f4',
+    paddingLeft: '0px'
+  }
 }));
 
 export default function EnhancedTable() {
@@ -198,7 +216,21 @@ export default function EnhancedTable() {
             <div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
                 <div class={"row " + classes.searchBar}>
                 <div class="col-xl-12 col-12 mb-xl-0">
-                    <h3 class="text-left mb-2 pt-3">Lịch sử giao dịch</h3>
+                <Breadcrumbs aria-label="breadcrumb" className={classes.breadcrumb}>
+                    <NavLink
+                      
+                      to={"/admin"}
+                      
+                      className={classes.link}
+                    >
+                      <HomeIcon className={classes.icon} />
+                      Trang chủ
+                    </NavLink>
+                    <Typography color="textPrimary" className={classes.link}>
+                      <GrainIcon className={classes.icon} />
+                      Lịch sử giao dịch
+                    </Typography>
+                  </Breadcrumbs>
                     <div className="mb-3 mt-3">
                     <div 
                         className="col-12" 
