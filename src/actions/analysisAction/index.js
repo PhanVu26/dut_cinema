4b6527f6ff1f7bf12a1672b571ab57f5071c6827
PHANVU-2601\DELL <phanvu200470@gmail.com>
2021-08-21 16:05:30 +0700

    Update sale chart page using data fetching

---
+++ src/actions/analysisAction/index.js
@@ -0,0 +1,47 @@
+import * as types from "../../constants/ActionType";
+import callApi from "../../utils/ApiCallerServer";
+
+export const actFetchDataMovieAnalysisRequest = () => {
+    return (dispatch) => {
+      return callApi("transactions/movie-analysis", "GET", null).then((res) => {
+        dispatch(actFetchDataMovieAnalysis(res.data));
+      });
+    };
+};
+
+export const actFetchDataMovieAnalysis = (data) => {
+    return {
+        type: types.MOVIE_ANALYSIS,
+        data
+    }
+}
+
+export const actFetchDataServiceAnalysisRequest = (query) => {
+    return (dispatch) => {
+      return callApi(`transactions/service-analysis${query}`, "GET", null).then((res) => {
+        dispatch(actFetchDataServiceAnalysis(res.data));
+      });
+    };
+};
+
+export const actFetchDataServiceAnalysis = (data) => {
+    return {
+        type: types.SERVICE_ANALYSIS,
+        data
+    }
+}
+
+export const actFetchDataSaleAnalysisRequest = (query) => {
+    return (dispatch) => {
+      return callApi(`transactions/sale-analysis${query}`, "GET", null).then((res) => {
+        dispatch(actFetchDataSaleAnalysis(res.data));
+      });
+    };
+};
+
+export const actFetchDataSaleAnalysis = (data) => {
+    return {
+        type: types.SALE_ANALYSIS,
+        data
+    }
+}
--- src/components/admin/Chart/SalesChart.js
+++ src/components/admin/Chart/SalesChart.js
@@ -11,163 +11,90 @@ import {
 } from "recharts";
 import { Pie, Line } from "react-chartjs-2";
 import { connect } from "react-redux";
-import * as actions from "../../../actions/transactionAction/index";
+import * as actions from "../../../actions/analysisAction/index";
 import TransactionTable from "./TransactionTable";
 
 class SalesChart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      transactions: {},
-      bookedQuanties: {},
-      saleData: {},
+      movieAnalysis: [],
+      serviceAnalist: [],
+      saleAnalysis: [],
+      lineChartOption: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false,
+              },
+            },
+          ],
+        },
+      }
     };
   }
 
-  static getDerivedStateFromProps(props, state) {
-    if (props.transactions) {
-      const tickets = props.transactions.filter((tran) => {
-        return tran.service === "Buy" || tran.service === "Book";
-      });
-      const transactions = props.transactions.reduce(
-        (prev, curr) => (
-          (prev[curr.service] = ++prev[curr.service] || 1), prev
-        ),
-        {}
-      );
-
-      // Tính số lượng vé trên mỗi phim
-      const bookedQuanties = tickets.reduce(
-        (prev, curr) => (
-          (prev[curr.ticket.showtime.movie.name] =
-            ++prev[curr.ticket.showtime.movie.name] || 1),
-          prev
-        ),
-        {}
-      );
-      const movieTickets = [];
-      const keys = Object.keys(bookedQuanties);
-      const values = Object.values(bookedQuanties);
-      for (let i = 0; i < keys.length; i++) {
-        movieTickets.push({
-          name: keys[i],
-          quantity: values[i],
-        });
-      }
-
-      // count sale data
-      const saleData = tickets.sort(function (a, b) {
-        return (
-          new Date(Date.parse(a.transaction_time)) -
-          new Date(b.transaction_time)
-        );
-      });
-
-      let sales = [];
-      let pre_time = "";
-      let money = 0;
-      for (let i = 0; i < saleData.length; i++) {
-        let item = saleData[i];
-        let time = item.transaction_time.slice(0, 7);
-        if (pre_time === time) {
-          if (item.service === "Buy") {
-            money += item.price;
-          } else {
-            money += item.price / 10;
-          }
-        } else {
-          let obj = {
-            time: pre_time,
-            money: money,
-          };
-          sales.push(obj);
-          pre_time = time;
-          if (item.service === "Buy") {
-            money += item.price;
-          } else {
-            money += item.price / 10;
-          }
-        }
-      }
-      let obj = {
-        time: pre_time,
-        money: money,
-      };
-      sales.push(obj);
-      sales.shift();
-      var months = [];
-      var salesData = [];
-      sales.forEach((data) => {
-        months.push(data.time);
-        salesData.push(data.money);
-      });
-
-      return {
-        transactions: {
-          labels: Object.keys(transactions),
-          datasets: [
-            {
-              label: "Trạng thái đặt vé",
-              data: Object.values(transactions),
-              backgroundColor: [
-                "rgba(253, 204, 69, 1)",
-                "rgba(38, 38, 205, 1)",
-                "rgba(255, 0, 0, 1)",
-                "rgba(75, 192, 192, 1)",
-              ],
-              borderColor: [
-                "rgba(253, 204, 69, 1)",
-                "rgba(38, 38, 205, 1)",
-                "rgba(255, 0, 0, 1)",
-                "rgba(75, 192, 192, 1)",
-              ],
-              borderWidth: 1,
-            },
+  componentDidMount(){
+    console.log("didmount")
+    this.props.fetchDataMovieAnalysis();
+    this.props.fetchDataSaleAnalysis("");
+    this.props.fetchDataServiceAnalysis("");
+  }
+  
+  mapToPieChartData(serviceAnalysis){
+    var services = [];
+    var quantity = [];
+    serviceAnalysis.forEach((data) => {
+      services.push(data.service);
+      quantity.push(data.total);
+    });
+    return {
+      labels: services,
+      datasets: [
+        {
+          label: "Trạng thái đặt vé",
+          data: quantity,
+          backgroundColor: [
+            "rgba(253, 204, 69, 1)",
+            "rgba(38, 38, 205, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(75, 192, 192, 1)",
           ],
-        },
-        bookedQuanties: movieTickets,
-        sales: {
-          labels: months,
-          datasets: [
-            {
-              label: "Doanh thu(vnđ)",
-              data: salesData,
-              fill: false,
-              backgroundColor: "rgb(255, 0, 0)",
-              borderColor: "rgba(255, 0, 0, 0.2)",
-            },
+          borderColor: [
+            "rgba(253, 204, 69, 1)",
+            "rgba(38, 38, 205, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(75, 192, 192, 1)",
           ],
+          borderWidth: 1,
         },
-      };
-    } else {
-      return {
-        transactions: {},
-        bookedQuanties: {},
-      };
+      ],
     }
   }
-
-  countSales = (arr) =>
-    arr.reduce(
-      (prev, curr) => (
-        (prev[curr.transaction_time] = prev[curr.transaction_time] || 1), prev
-      ),
-      {}
-    );
+  mapToLineChartData(saleAnalysis){
+    var months = [];
+    var salesData = [];
+    saleAnalysis.forEach((data) => {
+      months.push(data.month_year);
+      salesData.push(data.sumSales);
+    });
+    return {
+      labels: months,
+      datasets: [
+        {
+          label: "Doanh thu(vnđ)",
+          data: salesData,
+          fill: false,
+          backgroundColor: "rgb(255, 0, 0)",
+          borderColor: "rgba(255, 0, 0, 0.2)",
+        },
+      ],
+    }
+  }
   render() {
-    var { transactions, bookedQuanties, sales } = this.state;
-
-    const options = {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: false,
-            },
-          },
-        ],
-      },
-    };
+    var saleAnalysis = this.mapToLineChartData(this.props.saleAnalysis);
+    var serviceAnalysis = this.mapToPieChartData(this.props.serviceAnalysis);
     return (
       <div>
         <div className="row">
@@ -182,8 +109,8 @@ class SalesChart extends Component {
             <h4 className="mb-5 mt-3 text-center">
               Biểu đồ số lượng vé đã đặt theo phim
             </h4>
-            <BarChart width={1150} height={450} data={bookedQuanties}>
-              <XAxis dataKey="name" stroke="#8884d8" />
+            <BarChart width={1150} height={450} data={this.props.movieAnalysis}>
+              <XAxis dataKey="movieName" stroke="#8884d8" />
               <YAxis />
               <Tooltip wrapperStyle={{ width: 100, backgroundColor: "#ccc" }} />
               <Legend
@@ -198,7 +125,7 @@ class SalesChart extends Component {
                 }}
               />
               <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-              <Bar dataKey="quantity" fill="#8884d8" barSize={30} />
+              <Bar dataKey="buyQuantity" fill="#8884d8" barSize={30} />
             </BarChart>
           </div>
         </div>
@@ -214,7 +141,7 @@ class SalesChart extends Component {
             <h5 className="text-center mb-4 mt-3">
               Biểu đồ trạng thái giao dịch
             </h5>
-            <Pie data={transactions} />
+            <Pie data={serviceAnalysis} />
           </div>
           <div className="col-md-8">
             <TransactionTable></TransactionTable>
@@ -230,7 +157,7 @@ class SalesChart extends Component {
             }}
           >
             <h5 className="text-center mb-4 mt-3">Biểu đồ doanh thu</h5>
-            <Line data={sales} options={options} />
+            <Line data={saleAnalysis} options={this.state.lineChartOption} />
           </div>
         </div>
       </div>
@@ -239,14 +166,22 @@ class SalesChart extends Component {
 }
 const mapStateToProps = (state) => {
   return {
-    transactions: state.transactions.transactions,
+    movieAnalysis : state.analysisReducer.movieAnalysis,
+    serviceAnalysis: state.analysisReducer.serviceAnalysis,
+    saleAnalysis: state.analysisReducer.saleAnalysis
   };
 };
 
 const mapDispatchToProps = (dispatch, props) => {
   return {
-    fetchDataTransaction: () => {
-      dispatch(actions.actFetchDataTransactionsRequest());
+    fetchDataMovieAnalysis: () => {
+      dispatch(actions.actFetchDataMovieAnalysisRequest());
+    },
+    fetchDataServiceAnalysis: (query) => {
+      dispatch(actions.actFetchDataServiceAnalysisRequest(query));
+    },
+    fetchDataSaleAnalysis: (query) => {
+      dispatch(actions.actFetchDataSaleAnalysisRequest(query));
     },
   };
 };
