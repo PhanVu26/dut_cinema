db0026700c21bd19d5c71a0c198feb3bf29adffb
PHANVU-2601\DELL <phanvu200470@gmail.com>
2021-08-23 14:33:40 +0700

    Add unauth page

---
+++ src/pages/error/unauth/UnauthPage.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import "./UnauthPage.css";
+function UnAuthPage() {
+  return (
+    <div className="main">
+      <div id="particles" className="particles">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <main>
+        <section>
+          <h1>Page Not Found!</h1>
+          <div>
+            <span>4</span>
+            <span className="circle">0</span>
+            <span>3</span>
+          </div>
+          <p>
+            Xin lỗi, bạn không có quyền truy cập vào trang này.
+          </p>
+          <div>
+            <Link to="/">
+              <button>Quay lại trang chủ</button>
+            </Link>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default UnAuthPage;
